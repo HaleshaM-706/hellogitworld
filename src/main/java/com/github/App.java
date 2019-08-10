effcf2a948e77383cad05ab1a1f03a68eb7d21fa
Halesha M <halesha@inqude.com>
2019-08-10 18:28:15 +0530

    Changed the hello text

--- src/main/java/com/github/App.java
+++ src/main/java/com/github/App.java
@@ -5,12 +5,12 @@ package com.github;
  * Hello world!
  * Hello
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
     	//Comment
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello Halesha M!" );
     }
 }
 
