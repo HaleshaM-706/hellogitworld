b5042d242a608cfcf0c89d1cc5da96da41abe800
Halesha M <halesha@inqude.com>
2019-08-10 18:41:20 +0530

    Changes mentioned in the PR review

--- src/main/java/com/github/App.java
+++ src/main/java/com/github/App.java
@@ -10,7 +10,7 @@ public class App
     public static void main( String[] args )
     {
     	//Comment
-        System.out.println( "Hello Halesha M!" );
+        System.out.println( "Hello H!" );
     }
 }
 
